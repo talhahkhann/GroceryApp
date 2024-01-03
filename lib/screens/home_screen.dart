0632c796147f2e9501590209572ef2619b431250
talhahkhann <tk71577@gmail.com>
2024-01-03 12:51:25 +0500

    new commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:grocery_app/widgets/background_shade.dart';
+import 'package:grocery_app/widgets/product_card.dart';
 
 class Home_Screen extends StatelessWidget {
   const Home_Screen({super.key});
@@ -9,92 +9,99 @@ class Home_Screen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromRGBO(255, 255, 255, 1),
-        body: Column(children: [
-          SizedBox(
-            height: 160,
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.asset(
-                  'assets/images/vegi.png',
-                ),
-                Image.asset(
-                  "assets/images/Rectangle 17.png",
-                  fit: BoxFit.fill,
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 10, right: 10, top: 50),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Grocery Plus",
-                            style: GoogleFonts.poppins(
-                                textStyle: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 18)),
-                          ),
-                          Icon(Icons.notifications_outlined),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: [
-                        RawMaterialButton(
-                          onPressed: () {},
-                          elevation: 2.0,
-                          fillColor: Color.fromRGBO(135, 221, 57, 1),
-                          child: Icon(
-                            Icons.location_on_outlined,
-                            size: 20.0,
-                            color: Colors.white,
-                          ),
-                          padding: EdgeInsets.all(15.0),
-                          shape: CircleBorder(),
-                        ),
-                        Column(
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 160,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    'assets/images/vegi.png',
+                  ),
+                  Image.asset(
+                    "assets/images/Rectangle 17.png",
+                    fit: BoxFit.fill,
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10, right: 10, top: 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Padding(
-                              padding: EdgeInsets.only(right: 156),
-                              child: Text(
-                                "Your Location",
-                                style: GoogleFonts.poppins(),
-                              ),
-                            ),
                             Text(
-                              "32 Llanberis Close, Tonteg, CF38 1HR",
+                              "Grocery Plus",
                               style: GoogleFonts.poppins(
-                                  textStyle:
-                                      TextStyle(fontWeight: FontWeight.bold)),
+                                  textStyle: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18)),
                             ),
+                            Icon(Icons.notifications_outlined),
                           ],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          RawMaterialButton(
+                            onPressed: () {},
+                            elevation: 2.0,
+                            fillColor: Color.fromRGBO(135, 221, 57, 1),
+                            child: Icon(
+                              Icons.location_on_outlined,
+                              size: 20.0,
+                              color: Colors.white,
+                            ),
+                            padding: EdgeInsets.all(15.0),
+                            shape: CircleBorder(),
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(right: 156),
+                                child: Text(
+                                  "Your Location",
+                                  style: GoogleFonts.poppins(),
+                                ),
+                              ),
+                              Text(
+                                "32 Llanberis Close, Tonteg, CF38 1HR",
+                                style: GoogleFonts.poppins(
+                                    textStyle:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color.fromRGBO(240, 241, 241, 0.72),
-                hintText: "Search Anything",
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.all(Radius.circular(7.0)),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromRGBO(240, 241, 241, 0.72),
+                  hintText: "Search Anything",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
                 ),
               ),
             ),
-          ),
-        ]));
+            SizedBox(
+              height: 10,
+            ),
+            Product_Card(),
+          ]),
+        ));
   }
 }
